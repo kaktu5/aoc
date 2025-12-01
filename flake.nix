cce9278c43b5ffc8a6c53fa864f8b605e635de14
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-12-01 16:11:44 +0100

    refactor

--- flake.nix
+++ flake.nix
@@ -2,28 +2,42 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = {nixpkgs, ...}: let
-    inherit (nixpkgs) lib;
-    forEachSystem = systems: f: let
-      perSystemOutputs = lib.attrsets.genAttrs systems f;
-      outputTypes = perSystemOutputs |> lib.attrsets.attrValues |> lib.lists.head |> lib.attrsets.attrNames;
-    in
-      lib.attrsets.genAttrs outputTypes (outputType:
-        perSystemOutputs |> lib.attrsets.mapAttrs (_: attrs: attrs.${outputType}));
+    inherit (nixpkgs.lib.attrsets) attrValues mapAttrs recursiveUpdate;
+    inherit (nixpkgs.lib.lists) foldl';
+
+    mapSystems = systems: f: (foldl' (acc: system: (f system
+      |> mapAttrs (_: value: {${system} = value;})
+      |> recursiveUpdate acc)) {}
+    systems);
+    mapSystems' = mapSystems ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
   in
-    forEachSystem ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"] (system: let
+    mapSystems' (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
-        packages = lib.attrsets.attrValues {
-          inherit (pkgs) deadnix nil nixd statix;
-          inherit (pkgs.haskellPackages) fourmolu haskell-language-server;
+        name = "aoc-devshell";
+        packages = attrValues {
+          # nix tooling
+          inherit
+            (pkgs)
+            deadnix
+            nil
+            nixd
+            statix
+            ;
+          # haskell tooling
+          inherit
+            (pkgs.haskellPackages)
+            fourmolu
+            haskell-language-server
+            ;
           ghc' = pkgs.haskellPackages.ghcWithPackages (pkgs': [pkgs'.split]);
         };
       };
 
       formatter = pkgs.writeShellApplication {
-        name = "fmt";
-        runtimeInputs = lib.attrsets.attrValues {
+        name = "aoc-nix3-fmt-wrapper";
+        runtimeInputs = attrValues {
           inherit (pkgs) alejandra fd;
           inherit (pkgs.haskellPackages) fourmolu;
         };
